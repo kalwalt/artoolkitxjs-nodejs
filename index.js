9509074d07f17ea0dcf1f4099d2acf36fe509695
kalwalt <github@kalwaltart.it>
2020-01-24 23:59:48 +0100

    init repo with index.js and package.json

---
+++ index.js
@@ -0,0 +1,6 @@
+//const ARController = require("./artoolkitX.js/SDK/lib/artoolkitX.api.js");
+import artoolkitxjs from './node_modules/artoolkitX.js/SDK/lib/artoolkitX.api.js';
+
+
+console.log('Hello ArtoolkitX.js!');
+console.log(artoolkitxjs);
